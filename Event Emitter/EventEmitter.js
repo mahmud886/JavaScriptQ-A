9ca0f4943f77f5506801179ae2dc5d5866a19195
mahmud886 <iqbal886mahmud@gmail.com>
2024-01-13 13:52:46 +0600

    Event emitter js file updated

--- Event Emitter/EventEmitter.js
+++ Event Emitter/EventEmitter.js
@@ -1,36 +1,51 @@
-// You are free to use alternative approaches of
-// instantiating the EventEmitter as long as the
-// default export is correct.
-
 export default class EventEmitter {
-  constructor() {
-    throw 'Not implemented!';
-  }
+    constructor() {
+        // Avoid creating objects via `{}` to exclude unwanted properties
+        // on the prototype (such as `.toString`).
+        this._events = Object.create(null);
+    }
 
-  /**
-   * @param {string} eventName
-   * @param {Function} listener
-   * @returns {EventEmitter}
-   */
-  on(eventName, listener) {
-    throw 'Not implemented!';
-  }
+    on(eventName, listener) {
+        if (!Object.hasOwn(this._events, eventName)) {
+            this._events[eventName] = [];
+        }
 
-  /**
-   * @param {string} eventName
-   * @param {Function} listener
-   * @returns {EventEmitter}
-   */
-  off(eventName, listener) {
-    throw 'Not implemented!';
-  }
+        this._events[eventName].push(listener);
+        return this;
+    }
 
-  /**
-   * @param {string} eventName
-   * @param  {...any} args
-   * @returns {boolean}
-   */
-  emit(eventName, ...args) {
-    throw 'Not implemented!';
-  }
+    off(eventName, listener) {
+        // Ignore non-existing eventNames.
+        if (!Object.hasOwn(this._events, eventName)) {
+            return this;
+        }
+
+        const listeners = this._events[eventName];
+
+        // Find only first instance of the listener.
+        const index = listeners.findIndex((listenerItem) => listenerItem === listener);
+
+        if (index < 0) {
+            return this;
+        }
+
+        this._events[eventName].splice(index, 1);
+        return this;
+    }
+
+    emit(eventName, ...args) {
+        // Return false for non-existing eventNames or events without listeners.
+        if (!Object.hasOwn(this._events, eventName) || this._events[eventName].length === 0) {
+            return false;
+        }
+
+        // Make a clone of the listeners in case one of the listeners
+        // mutates this listener array.
+        const listeners = this._events[eventName].slice();
+        listeners.forEach((listener) => {
+            listener.apply(null, args);
+        });
+
+        return true;
+    }
 }
