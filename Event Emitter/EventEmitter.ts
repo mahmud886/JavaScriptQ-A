3152767c32bb4b32698e86efdecd92b20c3544aa
mahmud886 <iqbal886mahmud@gmail.com>
2024-01-14 21:29:24 +0600

    Event Emitter ts file added..

---
+++ Event Emitter/EventEmitter.ts
@@ -0,0 +1,59 @@
+interface IEventEmitter {
+    on(eventName: string, listener: Function): IEventEmitter;
+    off(eventName: string, listener: Function): IEventEmitter;
+    emit(eventName: string, ...args: Array<any>): boolean;
+}
+
+export default class EventEmitter implements IEventEmitter {
+    _events: Record<string, Array<Function>>;
+
+    constructor() {
+        // Avoid creating objects via `{}` to exclude unwanted properties
+        // on the prototype (such as `.toString`).
+        this._events = Object.create(null);
+    }
+
+    on(eventName: string, listener: Function): IEventEmitter {
+        if (!Object.hasOwn(this._events, eventName)) {
+            this._events[eventName] = [];
+        }
+
+        this._events[eventName].push(listener);
+        return this;
+    }
+
+    off(eventName: string, listener: Function): IEventEmitter {
+        // Ignore non-existing eventNames.
+        if (!Object.hasOwn(this._events, eventName)) {
+            return this;
+        }
+
+        const listeners = this._events[eventName];
+
+        // Find only first instance of the listener.
+        const index = listeners.findIndex((listenerItem) => listenerItem === listener);
+
+        if (index < 0) {
+            return this;
+        }
+
+        this._events[eventName].splice(index, 1);
+        return this;
+    }
+
+    emit(eventName: string, ...args: Array<any>): boolean {
+        // Return false for non-existing eventNames or events without listeners.
+        if (!Object.hasOwn(this._events, eventName) || this._events[eventName].length === 0) {
+            return false;
+        }
+
+        // Make a clone of the listeners in case one of the listeners
+        // mutates this listener array.
+        const listeners = this._events[eventName].slice();
+        listeners.forEach((listener) => {
+            listener.apply(null, args);
+        });
+
+        return true;
+    }
+}
